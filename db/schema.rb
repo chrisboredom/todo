1c3d1aeba77092796c4bc3d892858d305a23866e
Chris Mangano <chris.mangano@me.com>
2015-02-21 16:03:13 -0500

    Added todo model and association to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218012947) do
+ActiveRecord::Schema.define(version: 20150221204548) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string   "item"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
